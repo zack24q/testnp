c7cb69446592cc94c2313e22628e46873fed042a
zack <zack24q@gmail.com>
2017-07-28 10:39:39 +0800

    test

--- build/build.js
+++ build/build.js
@@ -1,8 +1,3 @@
-var fs = require('fs');
-fs.open("test.txt", "w", 0644, function (e, fd) {
-    if (e) throw e;
-    fs.write(fd, "first fs!", 0, 'utf8', function (e) {
-        if (e) throw e;
-        fs.closeSync(fd);
-    })
-});
+var fs = require('fs-extra');
+
+fs.copySync('/test1.js', '/test2.js');
