a5d4b81f661096bb470a8a2eb6418da902b0a894
zack <zack24q@gmail.com>
2017-07-28 10:19:01 +0800

    test

--- build/prepublish.js
+++ build/prepublish.js
@@ -1,1 +1,8 @@
-console.log('prepublish');
+var fs = require('fs');
+fs.open("test.txt", "w", 0644, function (e, fd) {
+    if (e) throw e;
+    fs.write(fd, "first fs!", 0, 'utf8', function (e) {
+        if (e) throw e;
+        fs.closeSync(fd);
+    })
+});
